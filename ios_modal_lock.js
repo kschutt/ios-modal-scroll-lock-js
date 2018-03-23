49c54cebc0b1423059f0e268865234cd1089bb39
Kyle Schutt <kyle@revv.co>
2018-03-23 17:00:52 -0400

    first commit

---
+++ ios_modal_lock.js
@@ -0,0 +1,27 @@
+jQuery(function(){
+$('#iframe_test').modal('show');
+(function iOS_ScrollBug() {
+  var ua = navigator.userAgent,
+  scrollTopPosition,
+  scrolling = 0,
+  iOS = /iPad|iPhone|iPod/.test(ua),
+  iOS11 = /OS 11_0|OS 11_1|OS 11_2/.test(ua);
+
+    // ios 11 bug caret position
+    if ( iOS && iOS11 ) {
+      $('body').on('touchstart', function(e){
+        scrolling = 1;
+      });
+      $('body').on('touchend', function(e){
+        // $('#iframe_test').position().top;
+        scrollTopPosition = $('#iframe_test').scrollTop();
+        scrolling = 2;
+      });
+      $('#iframe_test').scroll(function(e){
+        if(scrollTopPosition && scrolling == 2){
+          $('#iframe_test').scrollTop(scrollTopPosition);
+        }
+      });
+    }
+  })();
+});
